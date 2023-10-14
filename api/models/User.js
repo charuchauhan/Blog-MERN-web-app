e4c95fbf08997628e07b25bc4caf3bb19601be9f
Charu <charuchauhan587@gmail.com>
2023-10-14 11:24:00 +0530

    Initial commit

---
+++ api/models/User.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose")
+const UserSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        minlength:4,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    }
+})
+
+const UserModel=new mongoose.model("User",UserSchema);
+module.exports=UserModel;
